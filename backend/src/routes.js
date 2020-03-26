5e8161f706cc6715f0d78087d6969506df659477
Joao Otavio Chrisóstomo Barbosa <jobarbosa@sfiec.org.br>
2020-03-26 17:25:33 -0300

    Add incidents delete route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
@@ -9,5 +10,7 @@ routes.post('/ongs', OngController.create);
 routes.get('/ongs', OngController.index);
 routes.post('/incidents', IncidentController.create);
 routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
